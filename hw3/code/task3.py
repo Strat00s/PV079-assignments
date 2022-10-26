1cb863f3339d14ca8934505dc571bd50b9ae427c
Stratos <l.bastyr@seznam.cz>
2022-10-26 18:13:06 +0200

    task 3 done?

--- hw3/code/task3.py
+++ hw3/code/task3.py
@@ -1,77 +1,5 @@
 import requests
-from typing import Final, Optional
-from cryptography.hazmat.primitives.ciphers import algorithms, Cipher, modes
-import secrets
 import time
-
-#taken from referential encryption example
-BLOCK_LEN: Final[int] = 16
-
-def pad(msg: bytes) -> bytes:
-    """
-    Implements zero-byte padding. Will not pad message if BLOCK_LEN divides its length
-    Else will append necessary number of zero bytes
-
-    :param bytes msg: message to pad
-    :return bytes: padded message
-    """
-    n_bytes_to_padd = 0 if not len(msg) % BLOCK_LEN else BLOCK_LEN - (len(msg) % BLOCK_LEN)
-    return msg + bytes([0] * n_bytes_to_padd)
-
-def encrypt_ctr(key: bytes, nonce: bytes, msg: bytes) -> bytes:
-    """
-    AES-CTR encryption
-
-    :param bytes key: 16-byte long key
-    :param bytes nonce: 16-byte long nonce
-    :param bytes msg: plaintext to encrypt
-    :return bytes: the resulting ciphertext
-    """
-    encryptor = Cipher(algorithms.AES(key), modes.CTR(nonce)).encryptor()
-    return encryptor.update(msg) + encryptor.finalize()
-
-def decrypt_ctr(key: bytes, nonce: bytes, ciphertext: bytes) -> bytes:
-    """
-    AES-CTR decryption
-
-    :param bytes key: 16-byte long key
-    :param bytes nonce: 16-byte long nonce
-    :param bytes ciphertext: ciphertext to decrypt
-    :return bytes: the resulting plaintext
-    """
-    decryptor = Cipher(algorithms.AES(key), modes.CTR(nonce)).decryptor()
-    return decryptor.update(ciphertext) + decryptor.finalize()
-
-def get_cbc_mac(key: bytes, iv: bytes, msg: bytes) -> bytes:
-    """
-    Custom implementation of CBC-MAC driven by AES-128 cipher.
-
-    :param bytes key: 16-byte long key
-    :param bytes iv: 16-byte long IV
-    :param bytes msg: message to compute the MAC for
-    :return bytes: Resulting MAC, i.e. last 16 bytes of AES-CBC ciphertext
-    """
-    encryptor = Cipher(algorithms.AES(key), modes.CBC(iv)).encryptor()
-    ciphertext = encryptor.update(pad(msg)) + encryptor.finalize()
-    return ciphertext[-16:]
-
-def oracle_encrypt(key: bytes, msg: bytes, iv: Optional[bytes] = None) -> bytes:
-    """
-    This is a function that is run when `/encrypt` endpoint is called on pv079.fi.muni.cz
-
-    :param bytes key: Secret key, not an endpoint parameter.
-    :param bytes msg: message to encrypt
-    :param Optional[bytes] iv: IV to use, defaults to None
-    :return bytes: ciphertext
-    """
-    if not iv:
-        iv = secrets.token_bytes(BLOCK_LEN)
-
-    nonce = secrets.token_bytes(BLOCK_LEN)
-    ciphertext = encrypt_ctr(key, nonce, msg)
-    mac_tag = get_cbc_mac(key, iv, msg)
-    return nonce + ciphertext + iv + mac_tag
-
 
 
 def printHex(byte_data):
@@ -90,20 +18,8 @@ ctr   = encrypted_command[16:-32]
 iv    = encrypted_command[-32:-16]
 mac   = encrypted_command[-16:]
 
-print(f"Nonce ({len(nonce)}):")
-printHex(nonce)
-print(f"CTR ({len(ctr)}):")
-printHex(ctr)
-print(f"IV ({len(iv)}):")
-printHex(iv)
-print(f"MAC ({len(mac)}):")
-printHex(mac)
-
-#send last nonce with counter 0
-#we should get hash of it in MAC -> xor with first block of msg -> raw text?
-
 blocks = -(-len(ctr)//16)
-print(f"Blocks: {blocks}")
+print(f"CRT blocks: {blocks}")
 
 #decrypt message
 decrypted_msg = ""
@@ -140,12 +56,49 @@ if response.status_code != 200:
     print(f"Error ({response.status_code}): {response.raw}")
     exit(0)
 
-encrypted_msg = response.json()['result']
-print(f"Encrypted: {encrypted_msg}")
+forged_command = response.json()['result']
+print(f"Encrypted:\n  {forged_command}")
 
 send_response = requests.post(
     "https://pv079.fi.muni.cz/send",
-    data={"encrypted_command":encrypted_msg},)
+    data={"encrypted_command":forged_command},)
 print(send_response.json())
+
+
+#to get the same hash, we need to create IV from first block of plaintext, my plaintext and original IV
+plain_block = bytearray(decrypted_msg[:16], "utf-8")
+xor = arrayXor(iv, plain_block)
+forged_block = bytearray(forged_msg[:16], "utf-8")
+forged_iv = arrayXor(xor, forged_block)
+
+#resend with proper iv
+response = requests.get("https://pv079.fi.muni.cz/encrypt", params={"msg":forged_msg.encode("utf-8").hex(), "iv":forged_iv.hex()},)
+if response.status_code != 200:
+    print(f"Error ({response.status_code}): {response.raw}")
+    exit(0)
+
+forged_command = response.json()['result']
+print(f"Encrypted (proper iv):\n  {forged_command}")
+
+send_response = requests.post(
+    "https://pv079.fi.muni.cz/send",
+    data={"encrypted_command":forged_command},)
+print(send_response.json())
+
+
+#forged_command = bytearray.fromhex(forged_command)
+#nonce = forged_command[:16]
+#ctr   = forged_command[16:-32]
+#iv    = forged_command[-32:-16]
+#mac   = forged_command[-16:]
+#
+#print(f"Nonce ({len(nonce)}):")
+#printHex(nonce)
+#print(f"CTR ({len(ctr)}):")
+#printHex(ctr)
+#print(f"IV ({len(iv)}):")
+#printHex(iv)
+#print(f"MAC ({len(mac)}):")
+#printHex(mac)
 
 print("\ndone")

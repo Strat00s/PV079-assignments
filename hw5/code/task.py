7915adc1cebbe070eb79feab60f44ad899c1b5cb
stratos <l.bastyr@seznam.cz>
2022-11-27 00:04:53 +0100

    padding skip fix; still some problems

--- hw5/code/task.py
+++ hw5/code/task.py
@@ -4,9 +4,19 @@ from time import sleep
 
 BLOCK_SIZE = 16
 
-iv         = 'abcdef1234567890abcdef1234567890'
-ciphertext = '5ea5810e09cdeaee3d11c24dfd082d2bfd26349c5b75ecec82583cece3d11372f5db1b9420a9cbf228a98ba2d6b7bcbb86f070de15b1145d112b84e62d883d574e17e5f7ad480dbcebbf376cbf85bc27185091120f30fb6ee54623edb96594fc'
-plaintext  = "Modern cryptography is heavily based on mathematical theory and computer science practice"
+#iv         = 'abcdef1234567890abcdef1234567890'
+#ciphertext = '5ea5810e09cdeaee3d11c24dfd082d2bfd26349c5b75ecec82583cece3d11372f5db1b9420a9cbf228a98ba2d6b7bcbb86f070de15b1145d112b84e62d883d574e17e5f7ad480dbcebbf376cbf85bc27185091120f30fb6ee54623edb96594fc'
+#plaintext  = "Modern cryptography is heavily based on mathematical theory and computer science practice"
+
+#iv         = '4e17e5f7ad480dbcebbf376cbf85bc27'
+#ciphertext = '185091120f30fb6ee54623edb96594fc'
+
+
+#iv = "f6bc91b8beba35c4b0f1b46a8628e5b4"
+#ciphertext = "f6c4d8e9576ba1ea3df5ae9c191591051b11b9766a219f75bf8cefcffb7e67b35057e343feb200110e3c9c3f68ff1438c058c911ab0f2d53f2df782876777fe1e6fec92571eb5fb31c336d85b62846e3"
+
+iv = "c058c911ab0f2d53f2df782876777fe1"
+ciphertext = "e6fec92571eb5fb31c336d85b62846e3"
 
 
 # The method probes the web application with a modified ciphertext
@@ -39,27 +49,35 @@ plaintext = ""
 iv_ct_pairs = generatePairs(bytearray.fromhex(iv + ciphertext), BLOCK_SIZE)
 for iv, ct in iv_ct_pairs:
     msg = bytearray.fromhex("00" * BLOCK_SIZE)
+    blocktext = ""
     for i in reversed(range(0, BLOCK_SIZE)):
         padding = bytearray.fromhex("00" * (i + 1)  + f"{BLOCK_SIZE - i:02x}" * (BLOCK_SIZE - i - 1))
         print(f"Padding: {padding.hex()}")
         print(f"Message: {msg.hex()}")
         old = iv
         for c in range(0, 256):
+            #padding skip
+            if c ^ (BLOCK_SIZE - i) == BLOCK_SIZE - i:
+                continue
             msg[i] = c
             iv = arrayXor (iv, padding)
             iv = arrayXor (iv, msg)
             if probe(iv.hex(), ct.hex()):
-                print(f"{c ^ (BLOCK_SIZE - i):02x} {iv.hex()}  |  {plaintext}")
-                msg[i] = c ^ (BLOCK_SIZE - i)
+                c = c ^ (BLOCK_SIZE - i)
+                msg[i] = c
+                blocktext = chr(c) + blocktext
+                print(f"  {c:02x} ({chr(c) if chr(c).isprintable() else ' '}) {iv.hex()}  |  {plaintext}{blocktext}")
                 iv = old
                 break
             else:
-                print(f"{c:02x} {iv.hex()}  |  {plaintext}", end="\r")
+                c = c ^ (BLOCK_SIZE - i)
+                print(f"  {c:02x} ({chr(c) if chr(c).isprintable() else ' '}) {iv.hex()}  |  {plaintext}{blocktext}", end="\r")
             iv = old
-    print(bytes(msg))
-    plaintext += msg.decode("utf-8")
+        else:
+            print("\nSomething went wrong")
+            exit()
+    plaintext += blocktext
 print(f"Final plaintext: {plaintext}")
-
 
 
 

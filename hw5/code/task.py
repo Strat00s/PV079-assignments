a8722b61acacbf50c52231d8feb476f01c6b943c
stratos <l.bastyr@seznam.cz>
2022-11-27 00:58:30 +0100

    fully working

--- hw5/code/task.py
+++ hw5/code/task.py
@@ -15,9 +15,13 @@ BLOCK_SIZE = 16
 #iv = "f6bc91b8beba35c4b0f1b46a8628e5b4"
 #ciphertext = "f6c4d8e9576ba1ea3df5ae9c191591051b11b9766a219f75bf8cefcffb7e67b35057e343feb200110e3c9c3f68ff1438c058c911ab0f2d53f2df782876777fe1e6fec92571eb5fb31c336d85b62846e3"
 
+#iv = "5057e343feb200110e3c9c3f68ff1438"
+#ciphertext = "c058c911ab0f2d53f2df782876777fe1"
+
 iv = "c058c911ab0f2d53f2df782876777fe1"
 ciphertext = "e6fec92571eb5fb31c336d85b62846e3"
 
+#tanding8888888
 
 # The method probes the web application with a modified ciphertext
 # It returns True if padding is correct and False otherwise
@@ -50,29 +54,34 @@ iv_ct_pairs = generatePairs(bytearray.fromhex(iv + ciphertext), BLOCK_SIZE)
 for iv, ct in iv_ct_pairs:
     msg = bytearray.fromhex("00" * BLOCK_SIZE)
     blocktext = ""
+    pad_value = -1
     for i in reversed(range(0, BLOCK_SIZE)):
         padding = bytearray.fromhex("00" * (i + 1)  + f"{BLOCK_SIZE - i:02x}" * (BLOCK_SIZE - i - 1))
         print(f"Padding: {padding.hex()}")
         print(f"Message: {msg.hex()}")
+        print(f"IV:      {iv.hex()}")
+        print(pad_value)
         old = iv
         for c in range(0, 256):
             #padding skip
-            if c ^ (BLOCK_SIZE - i) == BLOCK_SIZE - i:
+            if c ^ (BLOCK_SIZE - i) == BLOCK_SIZE - i and c ^ (BLOCK_SIZE - i) != pad_value:
                 continue
             msg[i] = c
             iv = arrayXor (iv, padding)
             iv = arrayXor (iv, msg)
+            c = c ^ (BLOCK_SIZE - i)
             if probe(iv.hex(), ct.hex()):
-                c = c ^ (BLOCK_SIZE - i)
                 msg[i] = c
                 blocktext = chr(c) + blocktext
                 print(f"  {c:02x} ({chr(c) if chr(c).isprintable() else ' '}) {iv.hex()}  |  {plaintext}{blocktext}")
                 iv = old
+                #padding was found
+                if c >= 1 and c <= 16:
+                    pad_value = c
                 break
             else:
-                c = c ^ (BLOCK_SIZE - i)
                 print(f"  {c:02x} ({chr(c) if chr(c).isprintable() else ' '}) {iv.hex()}  |  {plaintext}{blocktext}", end="\r")
-            iv = old
+                iv = old
         else:
             print("\nSomething went wrong")
             exit()

99986f3e9e1e5dfecef6b536ad78701f94f78146
stratos <l.bastyr@seznam.cz>
2022-11-27 01:11:25 +0100

    fully working

--- hw5/code/task.py
+++ hw5/code/task.py
@@ -1,37 +1,17 @@
 from urllib import request, error
-from time import sleep
 
 
 BLOCK_SIZE = 16
 
-#iv         = 'abcdef1234567890abcdef1234567890'
-#ciphertext = '5ea5810e09cdeaee3d11c24dfd082d2bfd26349c5b75ecec82583cece3d11372f5db1b9420a9cbf228a98ba2d6b7bcbb86f070de15b1145d112b84e62d883d574e17e5f7ad480dbcebbf376cbf85bc27185091120f30fb6ee54623edb96594fc'
-#plaintext  = "Modern cryptography is heavily based on mathematical theory and computer science practice"
 
-#iv         = '4e17e5f7ad480dbcebbf376cbf85bc27'
-#ciphertext = '185091120f30fb6ee54623edb96594fc'
-
-
-#iv = "f6bc91b8beba35c4b0f1b46a8628e5b4"
-#ciphertext = "f6c4d8e9576ba1ea3df5ae9c191591051b11b9766a219f75bf8cefcffb7e67b35057e343feb200110e3c9c3f68ff1438c058c911ab0f2d53f2df782876777fe1e6fec92571eb5fb31c336d85b62846e3"
-
-#iv = "5057e343feb200110e3c9c3f68ff1438"
-#ciphertext = "c058c911ab0f2d53f2df782876777fe1"
-
-iv = "c058c911ab0f2d53f2df782876777fe1"
-ciphertext = "e6fec92571eb5fb31c336d85b62846e3"
-
-#tanding8888888
-
-# The method probes the web application with a modified ciphertext
-# It returns True if padding is correct and False otherwise
-# The first param must be a hex string
-# The second param must be a hex string
+iv = "cad9e05ad4bd01c5bec2ed6d480d5051"
+ciphertext = "5fcfe3189957b9c23f8a95e65a51ab74"
 
 
 def arrayXor(array_a, array_b):
     return bytearray([a ^ b for a, b in zip(array_a, array_b)])
 
+#generate array pairs which are used for attack
 def generatePairs(array, split_len):
     print(len(array))
     print(len(array) / split_len)
@@ -40,6 +20,7 @@ def generatePairs(array, split_len):
         result.append([array[i * split_len : (i + 1) * split_len], array[(i + 1) * split_len : (i + 2) * split_len]])
     return result
 
+#same as example
 def probe(iv, ciphertext):
     url = 'http://172.26.5.113/index.py?iv=' + iv + '&ciphertext=' + ciphertext
     try:
@@ -48,44 +29,51 @@ def probe(iv, ciphertext):
     except error.HTTPError:
         return False
 
+def printable(c):
+    return chr(c) if chr(c).isprintable() else ' '
+
 
 plaintext = ""
 iv_ct_pairs = generatePairs(bytearray.fromhex(iv + ciphertext), BLOCK_SIZE)
 for iv, ct in iv_ct_pairs:
-    msg = bytearray.fromhex("00" * BLOCK_SIZE)
-    blocktext = ""
-    pad_value = -1
+    msg       = bytearray.fromhex("00" * BLOCK_SIZE)    #current block plaintext "array"
+    blocktext = ""                                      #current block plaintext
+    pad_value = -1                                      #actuall padding retrieved from plaintext
+
     for i in reversed(range(0, BLOCK_SIZE)):
-        padding = bytearray.fromhex("00" * (i + 1)  + f"{BLOCK_SIZE - i:02x}" * (BLOCK_SIZE - i - 1))
+        padding = bytearray.fromhex("00" * (i + 1)  + f"{BLOCK_SIZE - i:02x}" * (BLOCK_SIZE - i - 1))   #desired padding "array"
         print(f"Padding: {padding.hex()}")
         print(f"Message: {msg.hex()}")
-        print(f"IV:      {iv.hex()}")
-        print(pad_value)
-        old = iv
+        old_iv = iv     #save iv for restoring in every round
         for c in range(0, 256):
+            c = c ^ (BLOCK_SIZE - i)    #xor immediataly as plain c is used a lot
+
             #padding skip
-            if c ^ (BLOCK_SIZE - i) == BLOCK_SIZE - i and c ^ (BLOCK_SIZE - i) != pad_value:
+            if c == BLOCK_SIZE - i and c != pad_value:
                 continue
-            msg[i] = c
+
+            #generate desired message by xoring padding and last message and current new character
+            msg[i] = c ^ (BLOCK_SIZE - i)
             iv = arrayXor (iv, padding)
             iv = arrayXor (iv, msg)
-            c = c ^ (BLOCK_SIZE - i)
+
+            #ask oracle
             if probe(iv.hex(), ct.hex()):
-                msg[i] = c
-                blocktext = chr(c) + blocktext
-                print(f"  {c:02x} ({chr(c) if chr(c).isprintable() else ' '}) {iv.hex()}  |  {plaintext}{blocktext}")
-                iv = old
-                #padding was found
+                #save actuall padding
                 if c >= 1 and c <= 16:
                     pad_value = c
+
+                msg[i]    = c                                                                #save character for next round
+                blocktext = printable(c) + blocktext                                         #save current block plaintext
+                print(f"  {c:02x} ({printable(c)}) {iv.hex()}  |  {plaintext}{blocktext}")   #somehwat pretty print
+                iv = old_iv                                                                  #restore iv
                 break
             else:
-                print(f"  {c:02x} ({chr(c) if chr(c).isprintable() else ' '}) {iv.hex()}  |  {plaintext}{blocktext}", end="\r")
-                iv = old
-        else:
-            print("\nSomething went wrong")
-            exit()
-    plaintext += blocktext
+                print(f"  {c:02x} ({printable(c)}) {iv.hex()}  |  {plaintext}{blocktext}", end="\r") #somehwat pretty print
+                iv = old_iv                                                                  #restore iv
+
+    plaintext += blocktext  #update entire plaintext
+
 print(f"Final plaintext: {plaintext}")
 
 
